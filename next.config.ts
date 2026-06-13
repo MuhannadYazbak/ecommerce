23be978e5fcdee31b64d14edfc727b13cb17c36a
MuhannadYazbak <yazbakm@gmail.com>
2026-06-13 14:20:37 +0300

    build: remove legacy module exports and fix next config syntax mismatch

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
-  // i18n: {
-  //   locales: ['en', 'ar', 'he'],
-  //   defaultLocale: 'en',
-  // }
+  /* If you have image domains or rewrites, they go here */
 };
-
 
 export default nextConfig;
-
-module.exports = {
-  experimental: {
-   turbopack: {
-      root: __dirname, // This tells Next.js: "Stop looking up! This folder is the real root."
-    },
-  }
-};
-
-
-// module.exports = {
-//   i18n: {
-//     locales: ['en', 'ar', 'he'],
-//     defaultLocale: 'en',
-//   },
-// };
